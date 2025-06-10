4dfb0d0658f74fb82abfdb6368a8f07cab0abfa8
Nicolas Leroux <hertford.fr@gmail.com>
2025-06-10 14:21:07 +0200

    fix: create method evaluate

--- src/main/java/com/cda/Algo5_PasswordStrength.java
+++ src/main/java/com/cda/Algo5_PasswordStrength.java
@@ -1,5 +1,8 @@
 package com.cda;
 
 public class Algo5_PasswordStrength {
-    // À toi ici d'écrire le code en TDD
+    public static String evaluate(String abc) {
+        // À toi ici d'écrire le code en TDD
+        throw new RuntimeException("Not yet implemented");
+    }
 }
